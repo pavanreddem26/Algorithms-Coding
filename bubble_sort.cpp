1120e67b167020a724f983cbf77e52dfbc654eae
pavanreddem26 <54822726+pavanreddem26@users.noreply.github.com>
2019-09-29 06:02:52 -0400

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j,k,n,a[n],flag;
+	flag=0;
+	printf("\n Enter the number of elements in teh array\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("\n Enter the elements in the array\n");
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				k=a[j];
+				a[j]=a[j+1];
+				a[j+1]=k;
+				flag=1;
+			}
+		}
+		if(flag==0)
+		{
+			break;
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("\n");
+		printf("%d",a[i]);		
+	}
+}
