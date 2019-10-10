de6b9b109bcd9eacdab92655006e0f74690f9102
pavanreddem26 <54822726+pavanreddem26@users.noreply.github.com>
2019-10-10 01:59:20 -0400

    Add files via upload

---
+++ 867.Transpose of the Matrix.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j,a[4][4],b[4][4],m,n,c;
+	printf("\n Enter the values of i\n");
+	scanf("%d",&i);
+	printf("\n Enter the values of j\n");
+	scanf("%d",&j);
+	for(m=0;m<i;m++)///Taking the valus as input
+	{
+		for(n=0;n<j;n++)
+		{
+			printf("\n Enter the values in the matrix\n");
+			scanf("%d",&a[m][n]);
+		}
+	}
+	for(m=0;m<i;m++)
+	{
+		for(n=0;n<j;n++)
+		{
+			printf("\n");
+			printf("%d",a[m][n]);
+		}
+	}
+	for(m=0;m<i;m++)//Transpose of the matrix
+	{
+		for(n=0;n<j;n++)
+		{
+			b[n][m]=a[m][n];
+		}
+	}
+	printf("\n");
+	printf("\n The transpose of the matrix is\n");
+	for(m=0;m<i;m++)
+	{
+		for(n=0;n<j;n++)
+		{
+			printf("\n");
+			printf("%d",b[m][n]);
+		}
+	}
+}
